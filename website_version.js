a2b9559d90b136853900bb4a7d14500368acdc94
MissNook <38181265+MissNook@users.noreply.github.com>
2021-03-10 23:32:13 +0100

    Affichage Résultat

--- website_version.js
+++ website_version.js
@@ -10,10 +10,60 @@ let embedMessage = {
     setTitle : function setTitle(newTitle){this.title = newTitle;},
     setColor : function setColor(newColor){this.color = newColor;}
 };
+let erreurSpan = document.getElementById("erreur");
+const tabEmojis = [{id="ladder", src="https://discord.com/assets/c9ab5c7dade3ae221f8b865b880d3f02.svg"}, 
+{id="beers", src="https://discord.com/assets/5e2ea03aa4963cda5e91d395c2587e6b.svg"}, 
+{id="archery", src="https://discord.com/assets/a33ea7f530f4b720a3a4c050594842fa.svg"}, 
+{id="six_pointed_star", src="https://discord.com/assets/ba9ded4c8399e915303b1f97974a1573.svg"}, 
+{id="leaves", src="https://discord.com/assets/40115dda7d9f615f9561a451236d9623.svg"}, 
+{id="first_place", src="https://discord.com/assets/e2f8f101328a4b4ae7875945716345b3.svg"}, 
+{id="second_place", src="https://discord.com/assets/c65da98dd1cd29756d4d5901ed549661.svg"}, 
+{id="third_place", src="https://discord.com/assets/9ecf90770f4de9be7b44cb601d49722c.svg"}];
+const svgImgHTML = '<img width="20px" height="20px" src="#src#"></img>';
+
+function afficherEmbedDansHTML(embedsToShow){
+    let divResultat = document.getElementById("resultat");
+    let titreRes = document.getElementById("TitreRes");
+    let descRes = document.getElementById("DescriptionRes");
+    let fieldsRes = document.getElementById("fieldsRes");
+    for(let i=0;i<embedsToShow.length;i++){
+        let currEmbed = embedsToShow[i];       
+        titreRes.innerHTML = changeEmojiIntoSvg(currEmbed.title);
+        descRes.innerHTML = currEmbed.description;
+        divResultat.style.border = "3px solid " + currEmbed.color;
+        for(let j=0;j<currEmbed.fields.length;j++){
+            let currField = currEmbed.fields[j];
+            var span = document.createElement("span");
+            span.innerHTML = changeEmojiIntoSvg(currField.name);
+            fieldsRes.appendChild(span);            
+            var span2 = document.createElement("span");
+            span2.innerHTML = changeEmojiIntoSvg(currField.value);
+            fieldsRes.appendChild(span2);
+        }
+    }
+}
+
+function changeEmojiIntoSvg(textToCheck){
+    var splitText = textToCheck.split(":");
+    let innerHTML = "";
+    if(splitText.length>1){
+        for(let i=0;i<splitText.length;i++){
+            for(let j=0;j<tabEmojis.length;j++){
+                if(splitText[i] == tabEmojis[j].id){
+                    innerHTML = svgImgHTML.replace("#src#", tabEmojis[j].src);                   
+                }else{
+                    innerHTML += splitText[i];
+                }
+            }
+        }
+    }else{
+        innerHTML += splitText[0];
+    }
+    return innerHTML;
+}
 
 function simulerTournoiCompetence(){
-    let formulaireTournoi = document.forms["formTournoiComp"];
-    let erreurSpan = formulaireTournoi["erreur"];
+    let formulaireTournoi = document.forms["formTournoiComp"];  
     let typeTournoiBtns = formulaireTournoi["competence"];
     let typeTournoi = "";
     for(let  i=0;i<typeTournoiBtns.length;i++){
@@ -33,7 +83,8 @@ function simulerTournoiCompetence(){
 	if(persoChoisis != "personne") {
 		persosCombat = PersoModule.getPersosFromArgs(persoChoisis, TournoiModule.persosTournoi);	
 	}
-    TournoiModule.simulerTournoi(embedMessage, typeTournoi, persosCombat, nbConcurrents);
+    let returnedEmbed = TournoiModule.simulerTournoi(embedMessage, typeTournoi, persosCombat, nbConcurrents);
+    afficherEmbedDansHTML([returnedEmbed]);
 }
 //obligé de faire comme ça car le module n'est pas visible dans le HTML contrairement à un code JS standard
 document.getElementById("btnValiderFormTournoiComp").onclick = simulerTournoiCompetence;
